1bec43e6d19a19c39c0141e62f04ab2b36b0bb41
Dan <Dan.donahue19@gmail.com>
2022-02-01 22:23:03 -0500

    add axios and moment to vue

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import moment from 'moment';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = 'http://localhost:8000/api';
+Object.defineProperty(Vue.prototype, '$moment', {value: moment});
+Object.defineProperty(Vue.prototype, '$axios', {value: axios});
 
 new Vue({
   vuetify,
